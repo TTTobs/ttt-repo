90d8d012330c05cb897013c6d1c2ec5060857ae3
TobiTobsTobs <tobiasjedlicka02@gmail.com>
2025-04-17 15:41:19 +0200

    Update ttt-setup.sh

--- ttt-setup.sh
+++ ttt-setup.sh
@@ -4,16 +4,17 @@
 ################################
 # HYPERLAND CONFIG FILES CURLS #
 ################################
-#pacman -S hyprland hyprpaper hyprlock
+pacman -S hyprland hyprpaper hyprlock
 
-echo "installed libs"
+config_path = "/home/ttt/.config/"
+hypr_path = "$config_path/hypr"
 
-mkdir -p /home/ttt/.config/hypr
-cd /home/ttt/.config/hypr
+mkdir -p "$hypr_path"
+cd "$hypr_path"
 
 echo $(pwd)
 
-curl -o hyprland.conf http://raw.githubusercontent.com/TTTobs/ttt-repo/refs/heads/main/hyprland.conf
-curl -o hyprlock.conf http://raw.githubusercontent.com/TTTobs/ttt-repo/refs/heads/main/hyprlock.conf
-curl -o hyprpaper.conf http://raw.githubusercontent.com/TTTobs/ttt-repo/refs/heads/main/hyprpaper.conf
+curl -o "$hypr_path/hyprland.conf" http://raw.githubusercontent.com/TTTobs/ttt-repo/refs/heads/main/hyprland.conf
+curl -o "$hypr_path/hyprlock.conf" http://raw.githubusercontent.com/TTTobs/ttt-repo/refs/heads/main/hyprlock.conf
+curl -o "$hypr_path/hyprpaper.conf" http://raw.githubusercontent.com/TTTobs/ttt-repo/refs/heads/main/hyprpaper.conf
 
