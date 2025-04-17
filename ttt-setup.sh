de4cac94267fcaf1a1a6a59df1df96c3bc53d92f
TobiTobsTobs <tobiasjedlicka02@gmail.com>
2025-04-17 14:38:04 +0200

    Create ttt-setup.sh

---
+++ ttt-setup.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+
+################################
+# HYPERLAND CONFIG FILES CURLS #
+################################
+pacman -S hyprland hyprpaper hyprlock
+
+mkdir -p /home/$USER/.config/hypr
+cd /home/$USER/.config/hypr
+
+curl http://raw.githubusercontent.com/TTTobs/ttt-repo/refs/heads/main/hyprland.conf > hyprland.conf
+curl http://raw.githubusercontent.com/TTTobs/ttt-repo/refs/heads/main/hyprlock.conf > hyprlock.conf
+curl curl http://raw.githubusercontent.com/TTTobs/ttt-repo/refs/heads/main/hyprpaper.conf > hyprpaper.conf
+
