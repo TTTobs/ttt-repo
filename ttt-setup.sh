2eff57954bbf85e2abd6b1775d08fe4f997cca55
TobiTobsTobs <tobiasjedlicka02@gmail.com>
2025-04-17 15:42:09 +0200

    Update ttt-setup.sh

--- ttt-setup.sh
+++ ttt-setup.sh
@@ -1,20 +1,27 @@
-#! /bin/bash
-
+#!/bin/bash
 
 ################################
 # HYPERLAND CONFIG FILES CURLS #
 ################################
+
+# Install the necessary packages
 pacman -S hyprland hyprpaper hyprlock
 
-config_path = "/home/ttt/.config/"
-hypr_path = "$config_path/hypr"
+# Define the configuration path and hypr folder path
+config_path="/home/ttt/.config"
+hypr_path="$config_path/hypr"
 
+# Create the hypr folder if it doesn't exist
 mkdir -p "$hypr_path"
+
+# Navigate to the hypr folder
 cd "$hypr_path"
 
+# Print the current working directory
 echo $(pwd)
 
-curl -o "$hypr_path/hyprland.conf" http://raw.githubusercontent.com/TTTobs/ttt-repo/refs/heads/main/hyprland.conf
-curl -o "$hypr_path/hyprlock.conf" http://raw.githubusercontent.com/TTTobs/ttt-repo/refs/heads/main/hyprlock.conf
-curl -o "$hypr_path/hyprpaper.conf" http://raw.githubusercontent.com/TTTobs/ttt-repo/refs/heads/main/hyprpaper.conf
+# Download the configuration files using curl
+curl -o "$hypr_path/hyprland.conf" https://raw.githubusercontent.com/TTTobs/ttt-repo/main/hyprland.conf
+curl -o "$hypr_path/hyprlock.conf" https://raw.githubusercontent.com/TTTobs/ttt-repo/main/hyprlock.conf
+curl -o "$hypr_path/hyprpaper.conf" https://raw.githubusercontent.com/TTTobs/ttt-repo/main/hyprpaper.conf
 
